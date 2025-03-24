a857ff0a9ee40c3bd7ac2241888b50a1d2529c60
peter <peter.arth5@gmail.com>
2025-03-24 11:54:01 +0000

    done 3 8 kyu questions on Codewars

---
+++ codewars/eight_kyu/you_cannot_code_under_pressure_no_1.js
@@ -0,0 +1,9 @@
+//  Mon, 24/03/25
+
+// 8. You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  return i * 2;
+}
