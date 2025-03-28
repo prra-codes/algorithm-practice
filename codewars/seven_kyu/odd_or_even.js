3b10d73c282a73198e7b5bdc333e8419e405687b
peter <peter.arth5@gmail.com>
2025-03-28 07:32:45 +0000

    done 3 7 kyu questions

---
+++ codewars/seven_kyu/odd_or_even.js
@@ -0,0 +1,45 @@
+//  Fri, 28/04/25
+
+// 19. Odd or Even?
+
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+function oddOrEven(array) {
+  if (array.length === 0) {
+    return 'even';
+  }
+
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+
+  if (total % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+}
+
+console.log(oddOrEven([0]));
+
+console.log(oddOrEven([0, 1, 4]));
+
+console.log(oddOrEven([0, -1, -5]));
